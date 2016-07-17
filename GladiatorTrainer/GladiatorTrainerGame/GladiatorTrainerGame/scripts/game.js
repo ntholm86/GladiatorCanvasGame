9aaf2d915c890d5d032c1ddc645a81653eb54a52
thsand <thyras33@hotmail.com>
2016-07-17 22:53:16 +0200

    its working!

--- GladiatorTrainer/GladiatorTrainerGame/GladiatorTrainerGame/scripts/game.js
+++ GladiatorTrainer/GladiatorTrainerGame/GladiatorTrainerGame/scripts/game.js
@@ -8,11 +8,11 @@ var Game;
             };
             this.InitiateFields = function () {
                 _this.Board.Fields.push();
-                _this.Board.Fields[0][0].Objects.push(new BoardObject("#000000"));
-                _this.Board.Fields[6][7].Objects.push(new BoardObject("#000000"));
-                _this.Board.Fields[9][7].Objects.push(new BoardObject("#cecece"));
-                _this.Board.Fields[6][5].Objects.push(new BoardObject("#32ba3e"));
-                _this.Board.Fields[1][7].Objects.push(new BoardObject("#3880e3"));
+                _this.Board.Fields[0][0].Object = new BoardObject("#000000");
+                _this.Board.Fields[6][7].Object = new BoardObject("#000000");
+                _this.Board.Fields[9][7].Object = new BoardObject("#cecece");
+                _this.Board.Fields[6][5].Object = new BoardObject("#32ba3e");
+                _this.Board.Fields[1][7].Object = new BoardObject("#3880e3");
             };
             this.Board = new Board();
             this.InitiateFields();
@@ -25,7 +25,7 @@ var Game;
         function Field(x, y) {
             this.xpos = x;
             this.ypos = y;
-            this.Objects = new Array();
+            this.Selected = false;
         }
         return Field;
     }());
@@ -42,7 +42,7 @@ var Game;
             var _this = this;
             this.Init = function () {
                 _this.Canvas = document.getElementById('hexmap');
-                //this.Canvas.addEventListener('click', this.FieldMarkerClickHandler, false);
+                _this.Canvas.addEventListener('click', _this.FieldMarkerClickHandler, false);
                 _this.BoardHeight = 10;
                 _this.BoardWidth = 10;
                 _this.HexagonAngle = 0.523598776;
@@ -56,18 +56,9 @@ var Game;
                     _this.Context.fillStyle = "#000000";
                     _this.Context.strokeStyle = "#CCCCCC";
                     _this.Context.lineWidth = 1;
-                    _this.DrawBoard(_this.Context, _this.BoardWidth, _this.BoardHeight);
                 }
             };
-            this.DrawBoard = function (canvasContext, width, height) {
-                var i, j;
-                for (i = 0; i < width; ++i) {
-                    for (j = 0; j < height; ++j) {
-                        _this.DrawHexagon(_this.Context, i * _this.HexRectangleWidth + ((j % 2) * _this.HexRadius), j * (_this.SideLength + _this.HexHeight), false, i, j);
-                    }
-                }
-            };
-            this.DrawHexagon = function (canvasContext, x, y, fill, xpos, ypos) {
+            this.DrawHexagon = function (canvasContext, x, y, fill, xpos, ypos, selected) {
                 var fill = fill || false;
                 canvasContext.beginPath();
                 canvasContext.moveTo(x + _this.HexRadius, y);
@@ -76,77 +67,54 @@ var Game;
                 canvasContext.lineTo(x + _this.HexRadius, y + _this.HexRectangleHeight);
                 canvasContext.lineTo(x, y + _this.SideLength + _this.HexHeight);
                 canvasContext.lineTo(x, y + _this.HexHeight);
-                canvasContext.font = "18pt Arial";
-                canvasContext.fillText(xpos + "," + ypos, x + _this.HexRadius - 15, y + _this.HexHeight + 25);
                 canvasContext.closePath();
                 if (fill) {
                     canvasContext.fill();
                 }
-                else {
-                    canvasContext.stroke();
-                }
+                canvasContext.stroke();
+                canvasContext.fillStyle = "#000000";
+                canvasContext.font = "18pt Arial";
+                canvasContext.fillText(xpos + "," + ypos, x + _this.HexRadius - 15, y + _this.HexHeight + 25);
             };
             this.DrawFields = function () {
                 _this.Context.clearRect(0, 0, _this.Canvas.width, _this.Canvas.height);
-                _this.DrawBoard(_this.Context, _this.BoardWidth, _this.BoardHeight);
                 for (var x = 0; x < _this.BoardWidth; ++x) {
                     for (var y = 0; y < _this.BoardHeight; ++y) {
-                        $.each(_this.Fields[x][y].Objects, function (i, object) {
-                            var screenX, screenY;
-                            screenX = x * _this.HexRectangleWidth + ((y % 2) * _this.HexRadius);
-                            screenY = y * (_this.HexHeight + _this.SideLength);
-                            if (x >= 0 && x < _this.BoardWidth) {
-                                if (y >= 0 && y < _this.BoardHeight) {
-                                    _this.Context.fillStyle = object.color;
-                                    _this.DrawHexagon(_this.Context, screenX, screenY, true, x, y);
+                        var screenX, screenY;
+                        screenX = x * _this.HexRectangleWidth + ((y % 2) * _this.HexRadius);
+                        screenY = y * (_this.HexHeight + _this.SideLength);
+                        if (x >= 0 && x < _this.BoardWidth) {
+                            if (y >= 0 && y < _this.BoardHeight) {
+                                var field = _this.Fields[x][y];
+                                if (field.Object) {
+                                    _this.Context.fillStyle = field.Object.color;
                                 }
+                                _this.DrawHexagon(_this.Context, screenX, screenY, true, x, y, false);
                             }
-                        });
+                        }
+                        _this.Context.fillStyle = "white";
                     }
                 }
             };
-            this.DrawMultipleBoardObjects = function (objects) {
-                $.each(objects, function (i, object) {
-                    var x, y, hexX, hexY, screenX, screenY;
-                    //if (object.xpos >= 0 && object.xpos < this.BoardWidth) {
-                    //    if (object.ypos >= 0 && object.ypos < this.BoardHeight) {
-                    //        this.Context.fillStyle = object.color;
-                    //        this.DrawHexagon(this.Context, screenX, screenY, true, object.xpos, object.ypos);
-                    //    }
-                    //}
-                });
-            };
-            this.DrawBoardObject = function (xin, yin, color) {
-                var x, y, hexX, hexY, screenX, screenY;
-                x = xin * _this.HexRectangleWidth;
-                y = yin * _this.HexRectangleHeight;
-                screenX = xin * _this.HexRectangleWidth + ((yin % 2) * _this.HexRadius);
-                screenY = yin * (_this.HexHeight + _this.SideLength);
-                _this.Context.clearRect(0, 0, _this.Canvas.width, _this.Canvas.height);
-                _this.DrawBoard(_this.Context, _this.BoardWidth, _this.BoardHeight);
-                // Check if the mouse's coords are on the board
-                if (xin >= 0 && xin < _this.BoardWidth) {
-                    if (yin >= 0 && yin < _this.BoardHeight) {
-                        _this.Context.fillStyle = color;
-                        _this.DrawHexagon(_this.Context, screenX, screenY, true, xin, yin);
-                    }
-                }
-            };
-            this.FieldClickHandler = function (eventInfo) {
+            this.FieldMarkerClickHandler = function (eventInfo) {
+                // 1) find ud af om der er nogen objecter på feltet
                 var x, y, hexX, hexY, screenX, screenY;
                 x = eventInfo.offsetX || eventInfo.layerX;
                 y = eventInfo.offsetY || eventInfo.layerY;
                 hexY = Math.floor(y / (_this.HexHeight + _this.SideLength));
                 hexX = Math.floor((x - (hexY % 2) * _this.HexRadius) / _this.HexRectangleWidth);
+                if (_this.Fields[hexX][hexY].Object != null) {
+                    console.log("object found on " + hexX + "," + hexY);
+                }
                 screenX = hexX * _this.HexRectangleWidth + ((hexY % 2) * _this.HexRadius);
                 screenY = hexY * (_this.HexHeight + _this.SideLength);
-                _this.Context.clearRect(0, 0, _this.Canvas.width, _this.Canvas.height);
-                _this.DrawBoard(_this.Context, _this.BoardWidth, _this.BoardHeight);
-                // Check if the mouse's coords are on the board
-                if (hexY >= 0 && hexY < _this.BoardHeight) {
-                    _this.Context.fillStyle = "#000000";
-                    _this.DrawHexagon(_this.Context, screenX, screenY, true, hexX, hexY);
-                }
+                var selectedField = _this.GetFieldByCoords(hexX, hexY);
+                selectedField.Selected = true;
+                _this.DrawFields();
+            };
+            this.GetFieldByCoords = function (x, y) {
+                var field = _this.Fields[x][y];
+                return field;
             };
             this.Init();
             this.Fields = [];
--- GladiatorTrainer/GladiatorTrainerGame/GladiatorTrainerGame/scripts/game.ts
+++ GladiatorTrainer/GladiatorTrainerGame/GladiatorTrainerGame/scripts/game.ts
@@ -15,34 +15,37 @@
 
         InitiateFields = () => {
             this.Board.Fields.push();
-            this.Board.Fields[0][0].Objects.push(new BoardObject("#000000"));
-            this.Board.Fields[6][7].Objects.push(new BoardObject("#000000"));
-            this.Board.Fields[9][7].Objects.push(new BoardObject("#cecece"));
-            this.Board.Fields[6][5].Objects.push(new BoardObject("#32ba3e"));
-            this.Board.Fields[1][7].Objects.push(new BoardObject("#3880e3"));
+            this.Board.Fields[0][0].Object = new BoardObject("#000000");
+            this.Board.Fields[6][7].Object = new BoardObject("#000000");
+            this.Board.Fields[9][7].Object = new BoardObject("#cecece");
+            this.Board.Fields[6][5].Object = new BoardObject("#32ba3e");
+            this.Board.Fields[1][7].Object = new BoardObject("#3880e3");
         }
     }
 
     export class Field {
         xpos: number;
         ypos: number;
-        Objects: BoardObject[];
+        Object: BoardObject;
+        Selected: boolean; 
 
         constructor(x: number, y: number) {
             this.xpos = x;
-            this.ypos = y;
-            this.Objects = new Array<BoardObject>();
+            this.ypos = y;     
+            this.Selected = false;               
         }
     }
 
     export class BoardObject {
         color: string;
+        Selectable: boolean;
 
         constructor(color: string) {
             this.color = color;
         }
 
     }
+
 
     export class Board {
         HexHeight: number;
@@ -72,7 +75,7 @@
 
         Init = () => {
             this.Canvas = <HTMLCanvasElement>document.getElementById('hexmap');
-            //this.Canvas.addEventListener('click', this.FieldMarkerClickHandler, false);
+            this.Canvas.addEventListener('click', this.FieldMarkerClickHandler, false);
             this.BoardHeight = 10;
             this.BoardWidth = 10;
             this.HexagonAngle = 0.523598776;
@@ -89,32 +92,13 @@
                 this.Context.fillStyle = "#000000";
                 this.Context.strokeStyle = "#CCCCCC";
                 this.Context.lineWidth = 1;
-
-                this.DrawBoard(this.Context, this.BoardWidth, this.BoardHeight);
+             
             }
         }
 
-        DrawBoard = (canvasContext: CanvasRenderingContext2D, width: number, height: number) => {
-            var i,
-                j;
 
-            for (i = 0; i < width; ++i) {
-                for (j = 0; j < height; ++j) {
-                    this.DrawHexagon(
-                        this.Context,
-                        i * this.HexRectangleWidth + ((j % 2) * this.HexRadius),
-                        j * (this.SideLength + this.HexHeight),
-                        false,
-                        i,
-                        j
-                    );
-                }
-            }
-        }
-
-        DrawHexagon = (canvasContext: CanvasRenderingContext2D, x: number, y: number, fill: boolean, xpos: number, ypos: number) => {
+        DrawHexagon = (canvasContext: CanvasRenderingContext2D, x: number, y: number, fill: boolean, xpos: number, ypos: number, selected: boolean) => {
             var fill = fill || false;
-
             canvasContext.beginPath();
             canvasContext.moveTo(x + this.HexRadius, y);
             canvasContext.lineTo(x + this.HexRectangleWidth, y + this.HexHeight);
@@ -122,96 +106,63 @@
             canvasContext.lineTo(x + this.HexRadius, y + this.HexRectangleHeight);
             canvasContext.lineTo(x, y + this.SideLength + this.HexHeight);
             canvasContext.lineTo(x, y + this.HexHeight);
-            canvasContext.font = "18pt Arial";
-            canvasContext.fillText(xpos + "," + ypos, x + this.HexRadius - 15, y + this.HexHeight + 25);
+         
+       
             canvasContext.closePath();
-
+            
+            
             if (fill) {
                 canvasContext.fill();
-            } else {
-                canvasContext.stroke();
-            }
+            } 
+
+            canvasContext.stroke();
+
+            canvasContext.fillStyle = "#000000";
+            canvasContext.font = "18pt Arial";
+            canvasContext.fillText(xpos + "," + ypos, x + this.HexRadius - 15, y + this.HexHeight + 25);
+           
         }
 
         DrawFields = () => {
 
             this.Context.clearRect(0, 0, this.Canvas.width, this.Canvas.height);
-            this.DrawBoard(this.Context, this.BoardWidth, this.BoardHeight);
+           
 
             for (var x = 0; x < this.BoardWidth; ++x) {
-                for (var y = 0; y < this.BoardHeight; ++y) {
+                for (var y = 0; y < this.BoardHeight; ++y) {                 
 
-                    $.each(this.Fields[x][y].Objects, (i, object) => {
+                    var screenX,
+                        screenY;
 
-                        var screenX,
-                            screenY;
+                    screenX = x * this.HexRectangleWidth + ((y % 2) * this.HexRadius);
+                    screenY = y * (this.HexHeight + this.SideLength);
 
-                        screenX = x * this.HexRectangleWidth + ((y % 2) * this.HexRadius);
-                        screenY = y * (this.HexHeight + this.SideLength);
+                    if (x >= 0 && x < this.BoardWidth) {
+                        if (y >= 0 && y < this.BoardHeight) {
+                            var field = this.Fields[x][y];
+                            if (field.Object) {
+                                this.Context.fillStyle = field.Object.color;
+                            } 
 
-                        if (x >= 0 && x < this.BoardWidth) {
-                            if (y >= 0 && y < this.BoardHeight) {
-                                this.Context.fillStyle = object.color;
-                                this.DrawHexagon(this.Context, screenX, screenY, true, x, y);
-                            }
+
+                            this.DrawHexagon(this.Context, screenX, screenY, true, x, y, false);
                         }
-                    });
+                    }
+                    this.Context.fillStyle = "white";
                 }
             }
         }
 
 
-        DrawMultipleBoardObjects = (objects: BoardObject[]) => {
-
-            $.each(objects, (i, object) => {
-                var x,
-                    y,
-                    hexX,
-                    hexY,
-                    screenX,
-                    screenY;
-
-                //if (object.xpos >= 0 && object.xpos < this.BoardWidth) {
-                //    if (object.ypos >= 0 && object.ypos < this.BoardHeight) {
-                //        this.Context.fillStyle = object.color;
-                //        this.DrawHexagon(this.Context, screenX, screenY, true, object.xpos, object.ypos);
-                //    }
-                //}
-
-            });
 
 
-        }
+
+      
 
 
-        DrawBoardObject = (xin: number, yin: number, color: string) => {
-            var x,
-                y,
-                hexX,
-                hexY,
-                screenX,
-                screenY;
+        FieldMarkerClickHandler = (eventInfo) => {
+            // 1) find ud af om der er nogen objecter på feltet
 
-            x = xin * this.HexRectangleWidth;
-            y = yin * this.HexRectangleHeight;
-
-            screenX = xin * this.HexRectangleWidth + ((yin % 2) * this.HexRadius);
-            screenY = yin * (this.HexHeight + this.SideLength);
-
-            this.Context.clearRect(0, 0, this.Canvas.width, this.Canvas.height);
-
-            this.DrawBoard(this.Context, this.BoardWidth, this.BoardHeight);
-
-            // Check if the mouse's coords are on the board
-            if (xin >= 0 && xin < this.BoardWidth) {
-                if (yin >= 0 && yin < this.BoardHeight) {
-                    this.Context.fillStyle = color;
-                    this.DrawHexagon(this.Context, screenX, screenY, true, xin, yin);
-                }
-            }
-        }
-
-        FieldClickHandler = (eventInfo) => {
 
             var x,
                 y,
@@ -222,63 +173,27 @@
 
             x = eventInfo.offsetX || eventInfo.layerX;
             y = eventInfo.offsetY || eventInfo.layerY;
-
-
             hexY = Math.floor(y / (this.HexHeight + this.SideLength));
             hexX = Math.floor((x - (hexY % 2) * this.HexRadius) / this.HexRectangleWidth);
+
+
+            if (this.Fields[hexX][hexY].Object != null ) {
+                console.log("object found on " + hexX + "," + hexY);
+            }
+
 
             screenX = hexX * this.HexRectangleWidth + ((hexY % 2) * this.HexRadius);
             screenY = hexY * (this.HexHeight + this.SideLength);
 
-            this.Context.clearRect(0, 0, this.Canvas.width, this.Canvas.height);
-
-            this.DrawBoard(this.Context, this.BoardWidth, this.BoardHeight);
-
-            // Check if the mouse's coords are on the board
-            if (hexY >= 0 && hexY < this.BoardHeight) {
-                this.Context.fillStyle = "#000000";
-                this.DrawHexagon(this.Context, screenX, screenY, true, hexX, hexY);
-            }
+            var selectedField = this.GetFieldByCoords(hexX, hexY);
+            selectedField.Selected = true;
+            this.DrawFields();
         }
 
-        //FieldMarkerClickHandler = (eventInfo) => {
-        //    // 1) find ud af om der er nogen objecter på feltet
-
-
-        //    var x,
-        //        y,
-        //        hexX,
-        //        hexY,
-        //        screenX,
-        //        screenY;
-
-        //    x = eventInfo.offsetX || eventInfo.layerX;
-        //    y = eventInfo.offsetY || eventInfo.layerY;
-
-
-        //    hexY = Math.floor(y / (this.HexHeight + this.SideLength));
-        //    hexX = Math.floor((x - (hexY % 2) * this.HexRadius) / this.HexRectangleWidth);
-
-
-        //    if (this.ObjectsOnBoard.filter(o => o.xpos == hexX && o.ypos == hexY).length > 0) {
-        //        console.log("object found on " + hexX + "," + hexY);
-        //        // this.Context.clearRect(hexX, hexY, this.Canvas.width, this.Canvas.height);
-        //    }
-
-
-        //    screenX = hexX * this.HexRectangleWidth + ((hexY % 2) * this.HexRadius);
-        //    screenY = hexY * (this.HexHeight + this.SideLength);
-
-        //    //this.Context.clearRect(0, 0, this.Canvas.width, this.Canvas.height);
-
-        //    //this.DrawBoard(this.Context, this.BoardWidth, this.BoardHeight);
-
-        //    //// Check if the mouse's coords are on the board
-        //    //if (hexY >= 0 && hexY < this.BoardHeight) {
-        //    //    this.Context.fillStyle = "#000000";
-        //    //    this.DrawHexagon(this.Context, screenX, screenY, true);
-        //    //}
-        //}
+        GetFieldByCoords = (x, y) => {
+            var field = this.Fields[x][y];
+            return field;
+        }
     }
 
 }
